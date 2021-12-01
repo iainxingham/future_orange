04f8369532c49b66cb10f198836d3ee02f52a1fd
Iain Crossingham <irc91914@gmail.com>
2021-12-01 21:08:17 +0000

    Ancillary parameters

--- src/Orange.elm
+++ src/Orange.elm
@@ -7,10 +7,13 @@ import Html.Events exposing (onInput)
 import String exposing (padLeft)
 
 type alias Model = 
-    { intConsultants: Int -- Total number of consultants
-    , intNoNights:    Int -- Number of people not doing nights
-    , intLeaveTime:   Int -- Leaving time on night shift after 8pm in half hours
-                          -- eg 3 = 2130  
+    { intConsultants: Int     -- Total number of consultants
+    , intNoNights:    Int     -- Number of people not doing nights
+    , intLeaveTime:   Int     -- Leaving time on night shift after 8pm in half hours
+                              -- eg 3 = 2130
+    , floatCalls:     Float   -- Number of calls per night
+    , intReturns:     Int     -- Ratio of nights with return to hospital
+    , floatReturnTime: Float   -- Time in hours when return to hospital        
     }
 
 convertLeaveTime : Int -> String
@@ -39,12 +42,18 @@ init =
   { intConsultants = 14
   , intNoNights = 1
   , intLeaveTime = 8
+  , floatCalls = 0.67
+  , intReturns = 9
+  , floatReturnTime = 3.0
   }
 
 type Msg
     = UpdateCons String
     | UpdateNoNights String
     | UpdateLeaveTime String
+    | UpdateCalls String
+    | UpdateReturns String
+    | UpdateReturnTime String
 
 update : Msg -> Model -> Model
 update msg model = 
@@ -57,6 +66,15 @@ update msg model =
             }
         UpdateLeaveTime new_val ->
             { model | intLeaveTime = String.toInt new_val |> Maybe.withDefault 0
+            }
+        UpdateCalls new_val ->
+            { model | floatCalls = String.toFloat new_val |> Maybe.withDefault 0.67 
+            }
+        UpdateReturns new_val ->
+            { model | intReturns = String.toInt new_val |> Maybe.withDefault 9
+            }
+        UpdateReturnTime new_val ->
+            { model | floatReturnTime = String.toFloat new_val |> Maybe.withDefault 3.0 
             }
 
 view : Model -> Html Msg
@@ -114,14 +132,46 @@ view model =
                             ] []
                         ]
                         , text <| convertLeaveTime model.intLeaveTime
-                    ]
-                    --[ p [] [ text "Typical leaving time (night shift):" ]
-                    --, p [] [ text "Average number of calls per night:" ]
+                    , div [] [
+                        text "Average number of calls per night:"
+                        , input
+                            [ type_ "range"
+                            , Html.Attributes.min "0"
+                            , Html.Attributes.max "5"
+                            , Html.Attributes.step "0.1"
+                            , value <| String.fromFloat model.floatCalls
+                            , onInput UpdateCalls
+                            ] []
+                        ]
+                        , text <| String.fromFloat model.floatCalls
+                    , div [] [
+                        text "Return to hospital every x nights:"
+                        , input
+                            [ type_ "range"
+                            , Html.Attributes.min "1"
+                            , Html.Attributes.max "21"
+                            , value <| String.fromInt model.intReturns
+                            , onInput UpdateReturns
+                            ] []
+                        ]
+                        , text <| String.fromInt model.intReturns
+                    , div [] [
+                        text "Typical number of hours present on emergency return:"
+                        , input
+                            [ type_ "range"
+                            , Html.Attributes.min "0.5"
+                            , Html.Attributes.max "5"
+                            , Html.Attributes.step "0.5"
+                            , value <| String.fromFloat model.floatReturnTime
+                            , onInput UpdateReturnTime
+                            ] []
+                        ]
+                        , text <| String.fromFloat model.floatReturnTime
                     --, p [] [ text "Return to hospital every x nights:" ]
                     --, p [] [ text "Typical number of hours present on emergency return:" ]
-                    --]
+                    ]
                 ]
-            ]
+            ] 
         , div [ class "container mt-5" 
             , id "results" ] 
             []
