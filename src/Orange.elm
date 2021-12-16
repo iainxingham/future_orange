4a3d65973d8f70dd78417da863debe78b69959b1
Iain Crossingham <irc91914@gmail.com>
2021-12-16 19:51:30 +0000

    Added model results table

--- src/Orange.elm
+++ src/Orange.elm
@@ -1,10 +1,12 @@
-module Orange exposing (main)
+module Orange exposing (main, nightsPerMonth)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
-import String exposing (padLeft)
+import String exposing (padLeft) 
+import Html.Events exposing (onClick)
+import Round exposing (round)
 
 type alias Model = 
     { intConsultants: Int     -- Total number of consultants
@@ -36,6 +38,51 @@ convertLeaveTime x =
     in
     hours ++ ":" ++ minutes
 
+nightsPerMonth : Model -> Float
+nightsPerMonth m =
+    let n = toFloat (m.intConsultants - m.intNoNights)
+
+    in 
+    365.25 / (12.0 * n)
+
+callinsPerYear : Model -> Float
+callinsPerYear m = 
+    let n = toFloat (m.intConsultants - m.intNoNights)
+        c = toFloat m.intReturns
+
+    in 
+    365.25 / (n * c) 
+
+weekendsNoNights : Model -> Float
+weekendsNoNights m = 
+    (/) 156.0 <| toFloat m.intConsultants
+
+weekendNights : Model -> Float
+weekendNights m = 
+    (/) 52.0 <| toFloat (m.intConsultants - m.intNoNights)
+
+weekendsAndNights : Model -> Float
+weekendsAndNights m =
+    (weekendsNoNights m) - (weekendNights m)
+
+nightPAs : Model -> Float
+nightPAs m = 
+    let
+        evening = (toFloat m.intLeaveTime) / 6
+        calls = m.floatCalls / 6
+        return = m.floatReturnTime / (3 * (toFloat m.intReturns))
+        each = 365.25 / (toFloat (m.intConsultants - m.intNoNights))
+    in
+    
+        (evening + calls + return) * each / 42
+
+weekendPAsNoNights : Model -> Float
+weekendPAsNoNights m = 
+    8 * (weekendsNoNights m) / 42
+
+weekendPAsNights : Model -> Float
+weekendPAsNights m =
+    8 * (weekendsAndNights m) / 42
 
 init : Model
 init =
@@ -54,6 +101,7 @@ type Msg
     | UpdateCalls String
     | UpdateReturns String
     | UpdateReturnTime String
+    | ResetButton
 
 update : Msg -> Model -> Model
 update msg model = 
@@ -75,6 +123,14 @@ update msg model =
             }
         UpdateReturnTime new_val ->
             { model | floatReturnTime = String.toFloat new_val |> Maybe.withDefault 3.0 
+            }
+        ResetButton ->
+            { intConsultants = 14
+            , intNoNights = 1
+            , intLeaveTime = 8
+            , floatCalls = 0.67
+            , intReturns = 9
+            , floatReturnTime = 3.0
             }
 
 view : Model -> Html Msg
@@ -167,14 +223,62 @@ view model =
                             ] []
                         ]
                         , text <| String.fromFloat model.floatReturnTime
-                    --, p [] [ text "Return to hospital every x nights:" ]
-                    --, p [] [ text "Typical number of hours present on emergency return:" ]
                     ]
                 ]
+                , div [class "col"
+                    , id "controls"
+                    ]
+                    [
+                        button [onClick ResetButton] [text "Reset"]
+                    ]
+                
             ] 
         , div [ class "container mt-5" 
             , id "results" ] 
-            []
+            [ table []
+                [ thead [] 
+                    [th [] []
+                    ,th [] [text "On nights rota"]
+                    ,th [] [text "No nights"]
+                    ]
+                , tr []
+                    [td [] [text "Number of nights per month"]
+                    ,td [] [text <| Round.round 2 (nightsPerMonth model)]
+                    ,td [] [text "0"]
+                    ]
+                , tr []
+                    [td [] [text "Approximate number of overnight call ins per year"]
+                    ,td [] [text <| Round.round 2 (callinsPerYear model)]
+                    ,td [] [text "0"]
+                    ]
+                , tr []
+                    [td [] [text "Daytime weekends per year"]
+                    ,td [] [text <| Round.round 2 (weekendsAndNights model)]
+                    ,td [] [text <| Round.round 2 (weekendsNoNights model)]
+                    ]
+                , tr []
+                    [td [] [text "Nighttime weekends per year"]
+                    ,td [] [text <| Round.round 2 (weekendNights model)]
+                    ,td [] [text "0"]
+                    ]
+                , tr []
+                    [td [] [text "Night shift PAs (per week)"]
+                    ,td [] [text <| Round.round 2 (nightPAs model)]
+                    ,td [] [text "0"]
+                    ]
+                , tr []
+                    [td [] [text "Weekend days PAs (per week)"]
+                    ,td [] [text <| Round.round 2 (weekendPAsNights model)]
+                    ,td [] [text <| Round.round 2 (weekendPAsNoNights model)]
+                    ]
+                , tr []
+                    [td [] [text "Total out of hours PAs (per week)"]
+                    ,td [] [text <| Round.round 2 ((nightPAs model) + (weekendPAsNights model))]
+                    ,td [] [text <| Round.round 2 (weekendPAsNoNights model)]
+                    ]                    
+                ]
+                
+            ] -- Add model validation here?
         ]
 
 main : Program () Model Msg
